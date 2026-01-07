381f904af36557ca23a2c081aa75a61e12e81d19
eduviga <eviganotti@gmail.com>
2026-01-07 12:14:45 -0300

    Update app.js

--- HonoShop/app.js
+++ HonoShop/app.js
@@ -1,3 +1,6 @@
-function agregarItem() {
-  alert("clic en Agregar");
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnAgregar");
+  btn.addEventListener("click", () => {
+    alert("clic en Agregar");
+  });
+});
