85d9d15bc8b19dee62ad4c725d898482f840f63e
eduviga <eviganotti@gmail.com>
2026-02-28 20:41:37 -0300

    Update group-info.js

--- functions/api/shopping/group-info.js
+++ functions/api/shopping/group-info.js
@@ -1,1 +1,22 @@
+export async function onRequestGet({ env, request }) {
 
+  const url = new URL(request.url);
+  const groupId = url.searchParams.get("groupId");
+
+  if (!groupId) {
+    return Response.json({ ok:false });
+  }
+
+  const group = await env.DB.prepare(
+    "SELECT code FROM groups WHERE id=?"
+  ).bind(groupId).first();
+
+  if (!group) {
+    return Response.json({ ok:false });
+  }
+
+  return Response.json({
+    ok:true,
+    code: group.code
+  });
+}
